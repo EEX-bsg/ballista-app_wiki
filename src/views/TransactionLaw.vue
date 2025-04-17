ba01f71ca9861f907d9ba28ce30a7de3f5bd2fd9
wkbys249 <wkbys249@gmail.com>
2025-04-17 20:23:22 +0900

    update law document

--- src/views/TransactionLaw.vue
+++ src/views/TransactionLaw.vue
@@ -18,8 +18,8 @@ import DefaultLayout from '../layouts/DefaultLayout.vue';
                                     <template v-slot:prepend>
                                         <v-icon icon="mdi-store" color="primary"></v-icon>
                                     </template>
-                                    <v-list-item-title class="font-weight-bold">販売事業者・責任者</v-list-item-title>
-                                    <v-list-item-subtitle>EEX-slime ※実名は請求があれば遅滞なく開示します</v-list-item-subtitle>
+                                    <v-list-item-title class="font-weight-bold">サービス責任者</v-list-item-title>
+                                    <v-list-item-subtitle>若林悠斗 (a.k.a. EEX-slime)</v-list-item-subtitle>
                                 </v-list-item>
 
                                 <v-divider></v-divider>
@@ -56,10 +56,21 @@ import DefaultLayout from '../layouts/DefaultLayout.vue';
 
                                 <v-list-item>
                                     <template v-slot:prepend>
+                                        <v-icon icon="mdi-message-text" color="primary"></v-icon>
+                                    </template>
+                                    <v-list-item-title class="font-weight-bold">その他の連絡先</v-list-item-title>
+                                    <v-list-item-subtitle>Discord: eex_slime</v-list-item-subtitle>
+                                    <v-list-item-subtitle>X(Twitter): @EEX_slime</v-list-item-subtitle>
+                                </v-list-item>
+
+                                <v-divider></v-divider>
+
+                                <v-list-item>
+                                    <template v-slot:prepend>
                                         <v-icon icon="mdi-credit-card" color="primary"></v-icon>
                                     </template>
                                     <v-list-item-title class="font-weight-bold">お支払い方法</v-list-item-title>
-                                    <v-list-item-subtitle>Stripeが対応する決済手段(クレジットカードを想定)</v-list-item-subtitle>
+                                    <v-list-item-subtitle>Stripeが対応する決済手段(クレジットカードを想定・開発中につき変更する場合があります)</v-list-item-subtitle>
                                 </v-list-item>
 
                                 <v-divider></v-divider>
@@ -69,7 +80,7 @@ import DefaultLayout from '../layouts/DefaultLayout.vue';
                                         <v-icon icon="mdi-truck-delivery" color="primary"></v-icon>
                                     </template>
                                     <v-list-item-title class="font-weight-bold">引き渡し時期</v-list-item-title>
-                                    <v-list-item-subtitle>決済完了後、サーバーに反映され次第、即座に利用いただけます。</v-list-item-subtitle>
+                                    <v-list-item-subtitle>決済完了後、Ballista appの管理サーバーに反映され次第、利用いただけます。</v-list-item-subtitle>
                                 </v-list-item>
 
                                 <v-divider></v-divider>
@@ -79,8 +90,43 @@ import DefaultLayout from '../layouts/DefaultLayout.vue';
                                         <v-icon icon="mdi-calendar-refresh" color="primary"></v-icon>
                                     </template>
                                     <v-list-item-title class="font-weight-bold">返品・キャンセルについて</v-list-item-title>
-                                    <v-list-item-subtitle>デジタルコンテンツの性質上、購入後の返品・返金はお受けしておりません。</v-list-item-subtitle>
+                                    <v-list-item-subtitle>デジタルコンテンツの性質上、購入後の返品・返金は、原則としてお受けしておりません。</v-list-item-subtitle>
                                     <v-list-item-subtitle>ただし、サブスクリプションはいつでも解約可能です。解約後は次回更新日以降の課金は発生しません。</v-list-item-subtitle>
+                                </v-list-item>
+
+                                <v-divider></v-divider>
+
+                                <v-list-item>
+                                    <template v-slot:prepend>
+                                        <v-icon icon="mdi-monitor" color="primary"></v-icon>
+                                    </template>
+                                    <v-list-item-title class="font-weight-bold">システム要件</v-list-item-title>
+                                    <v-list-item-subtitle>Windows11, macOS Sequoia, Ubuntu 24.04LTSをサポート対象とする予定です。(開発中につき変更される場合があります)</v-list-item-subtitle>
+                                    <v-list-item-subtitle>それ以外でのOSでの動作は担保しません。大抵の環境で動くと思いますが、自己責任でお願いします。</v-list-item-subtitle>
+                                    <v-list-item-subtitle>また今後のBallista appのアップデートにより、システム要件が更新される場合があります。ご了承ください。</v-list-item-subtitle>
+                                </v-list-item>
+                                
+                                <v-divider></v-divider>
+                                
+                                <v-list-item>
+                                    <template v-slot:prepend>
+                                        <v-icon icon="mdi-restart" color="primary"></v-icon>
+                                    </template>
+                                    <v-list-item-title class="font-weight-bold">サブスクリプション契約の自動更新について</v-list-item-title>
+                                    <v-list-item-subtitle>月間・年間のサブスクリプション契約の場合、サブスクリプションは自動更新されます。</v-list-item-subtitle>
+                                    <v-list-item-subtitle>月間契約の場合は、契約から30日後、年間契約の場合は、契約から365日後が更新日となります。(開発中につき変更される場合があります)</v-list-item-subtitle>
+                                </v-list-item>
+
+                                <v-divider></v-divider>
+
+                                <v-list-item>
+                                    <template v-slot:prepend>
+                                        <v-icon icon="mdi-restart-off" color="primary"></v-icon>
+                                    </template>
+                                    <v-list-item-title class="font-weight-bold">サブスクリプション契約の停止について</v-list-item-title>
+                                    <v-list-item-subtitle>サブスクリプションを停止したい場合は、更新日前にキャンセルを忘れずに行ってください。</v-list-item-subtitle>
+                                    <v-list-item-subtitle>契約更新日前にユーザー宛に何かしらの通知を送る予定です。(開発中により方法は検討中)</v-list-item-subtitle>
+                                    <v-list-item-subtitle>サブスクリプション契約停止後も、次回更新日まではサブスクリプションサービスを受け続けられます。</v-list-item-subtitle>
                                 </v-list-item>
 
                                 <v-divider></v-divider>
